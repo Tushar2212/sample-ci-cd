b237446bbd03155352efc6a08100157896d8b2a9
Tushar Sharma <tushar.work22@gmail.com>
2021-04-18 04:32:24 +0530

    deploy test

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.red,
       ),
       home: MyHomePage(title: 'Ci-Cd sample'),
     );
